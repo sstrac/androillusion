def6a22619e43baea198a056077bf86aebd2306b
sstrac <s.r.strachan@outlook.com>
2025-04-03 15:37:34 +0100

    customisable text label for number

--- number.gd
+++ number.gd
@@ -1,13 +1,14 @@
 extends Control
 
+
+@export var rich_text_label: RichTextLabel
 @export var max_font_size: int
 @export var min_font_size: int
 @export var rise_height: int
 @export var lock_time_s: int
 @export var lifetime_s: int
 @export var increment_gradient: Gradient
-	
-@onready var label: RichTextLabel = get_node("%RichTextLabel")
+
 @onready var anim: AnimationPlayer = get_node("AnimationPlayer")
 @onready var lock_timer: Timer = get_node("LockTimer")
 @onready var free_timer: Timer = get_node("FreeTimer")
@@ -18,13 +19,24 @@ var locked = false
 
 
 func _ready():
+	if rich_text_label == null:
+		rich_text_label = RichTextLabel.new()
+		rich_text_label.scroll_active = false
+		rich_text_label.fit_content = true
+		rich_text_label.autowrap_mode = TextServer.AUTOWRAP_OFF
+		rich_text_label.bbcode_enabled = true
+	
+	get_node("HBoxContainer").add_child(rich_text_label)
+	_update_label()
+		
+		
 	lock_timer.wait_time = lock_time_s
 	free_timer.wait_time = lifetime_s
 
 	
 func _process(_delta):
-	if label.custom_minimum_size.y < rise_height:
-		label.custom_minimum_size.y += 1
+	if rich_text_label.custom_minimum_size.y < rise_height:
+		rich_text_label.custom_minimum_size.y += 1
 	
 
 func _append_value(amount):
@@ -36,11 +48,11 @@ func _append_value(amount):
 	
 	
 func _update_label():
-	label.add_theme_font_size_override('normal_font_size', self.font_size)
-	label.set_text("[center]%d" % self.value)
+	rich_text_label.add_theme_font_size_override('normal_font_size', self.font_size)
+	rich_text_label.set_text("[center]%d" % self.value)
 	
 	var font_size_over_max: float = float(font_size - min_font_size) / float(max_font_size - min_font_size)
-	label.add_theme_color_override('default_color', increment_gradient.sample(font_size_over_max))
+	rich_text_label.add_theme_color_override('default_color', increment_gradient.sample(font_size_over_max))
 
 
 func bump(amount):
